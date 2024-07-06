dadde95c2226bc7f6b1ff347e9eca0e49b0954ba
BnKeTia <bernardnketia51@gmail.com>
2024-07-06 14:37:13 +0000

    edited index.js file to fix import statements

--- daavi-special/src/index.js
+++ daavi-special/src/index.js
@@ -4,11 +4,11 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDom.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
