8e438205034e2215bf65e4e4aad9d67361d597a1
BnKeTia <bernardnketia51@gmail.com>
2024-07-17 14:49:29 +0000

    added team member profiles to the about page

---
+++ src/components/Layout.jsx
@@ -0,0 +1,36 @@
+// src/components/Layout.js
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import Header from "./Header.js";
+import Footer from "./Footer.js";
+import Cookies from "js-cookie";
+
+const Layout = ({ children, title = 'Daavi Special', content, type, name, description }) => {
+    const username = Cookies.get('username');
+    const isAuthenticated = Cookies.get('isAuthenticated');
+    
+    return (
+        <>
+            <HelmetProvider>
+                <Helmet>
+                    <title>{title}</title>
+                    <meta name="description" content={content} />
+                    <meta name="description" content={description} />
+                    <meta property="og:type" content={type} />
+                    <meta property="og:title" content={title} />
+                    <meta property="og:description" content={description} />
+                    <meta name="twitter:creator" content={name} />
+                    <meta name="twitter:card" content={type} />
+                    <meta name="twitter:title" content={title} />
+                    <meta name="twitter:description" content={description} />
+                </Helmet>
+            </HelmetProvider>
+            <Header />
+            <div className="grid min-h-screen gap-0 bg-blue-5">
+                {children}
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default Layout;
